c45e1dfbc3cfcc6d677134eab6432c9423c7bc7a
Jakub Vykydal <Vykydal.Jakub@stud.vassboskovice.cz>
2022-04-19 10:46:25 +0200

    k

--- OOP_Kostky/MainWindow.xaml.cs
+++ OOP_Kostky/MainWindow.xaml.cs
@@ -35,11 +35,23 @@ namespace OOP_Kostky
             InitializeComponent();
         }
 
-        
+        private void ZobrazKostky()
+        {
+            Kostka1.Content = kostky[0].Hodnota;
+            Kostka2.Content = kostky[1].Hodnota;
+            Kostka3.Content = kostky[2].Hodnota;
+            Kostka4.Content = kostky[3].Hodnota;
+            Kostka5.Content = kostky[4].Hodnota;
+            Kostka6.Content = kostky[5].Hodnota;
 
+        }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            foreach(var kostka in kostky)
+            {
+                kostka.Hod();
+            }
+            ZobrazKostky();
         }
     }
 }
